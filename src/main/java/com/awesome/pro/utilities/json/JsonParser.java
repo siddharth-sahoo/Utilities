abd06a8fdf9b031f44f7218d5aafec94739307a7
Siddharth Sourav Sahoo <siddharth.s@247-inc.com>
2014-12-08 13:21:27 +0530

    Changed JSON parser init

--- src/main/java/com/awesome/pro/utilities/json/JsonParser.java
+++ src/main/java/com/awesome/pro/utilities/json/JsonParser.java
@@ -7,12 +7,18 @@ import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
+import com.awesome.pro.utilities.network.HTTPFactory;
+import com.awesome.pro.utilities.network.IHTTPPerformance;
+import com.awesome.pro.utilities.network.IHTTPRequest;
+import com.awesome.pro.utilities.network.IHTTPResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializer;
 
 /**
@@ -39,7 +45,7 @@ public class JsonParser {
 	/**
 	 * @param deserializers Adapters for deserialization.
 	 * @param serializersAdapters for serialization.
-	 * @return
+	 * @return Instance of JSON parser.
 	 */
 	public static JsonParser getInstance(
 			final Map<Class<?>, JsonDeserializer<Class<?>>> deserializers,
@@ -64,7 +70,16 @@ public class JsonParser {
 	 */
 	private JsonParser(final Map<Class<?>, JsonDeserializer<Class<?>>> deserializers,
 			final Map<Class<?>, JsonSerializer<Class<?>>> serializers) {
-		GsonBuilder builder = new GsonBuilder();
+		GsonBuilder builder = new GsonBuilder().
+				registerTypeAdapter(IHTTPRequest.class,
+						Holder.REQUEST_ADAPTER).
+				registerTypeAdapter(IHTTPResponse.class,
+						Holder.RESPONSE_ADAPTER).
+				registerTypeAdapter(IHTTPPerformance.class,
+						Holder.PERFORMANCE_ADAPTER).
+				registerTypeAdapter(JsonElement.class,
+						Holder.JSON_ELEMENT_ADAPTER);
+
 
 		if (deserializers != null) {
 			final Iterator<Entry<Class<?>, JsonDeserializer<Class<?>>>> iter1 =
@@ -177,6 +192,82 @@ public class JsonParser {
 
 	public <T> T fromJson(String json, Type typeOfT) {
 		return this.gson.fromJson(json, typeOfT);
+	}
+
+	/**
+	 * Holder class for GSON and adapter instances which will contain
+	 * it in a thread safe manner.
+	 * @author siddharth.s
+	 */
+	private final static class Holder {
+
+		private final static JsonDeserializer<IHTTPRequest> REQUEST_ADAPTER
+		= new HTTPRequestAdapter();
+
+		private final static JsonDeserializer<IHTTPResponse> RESPONSE_ADAPTER
+		= new HTTPResponseAdapter();
+
+		private final static JsonDeserializer<IHTTPPerformance> PERFORMANCE_ADAPTER
+		= new HTTPPerformanceAdapter();
+
+		private final static JsonDeserializer<JsonElement> JSON_ELEMENT_ADAPTER
+		= new JsonElementAdapter();
+
+		/**
+		 * Adapter class for GSON deserialization that maps the IHTTPRequest
+		 * interface with its implementation.
+		 * @author siddharth.s
+		 */
+		private final static class HTTPRequestAdapter implements JsonDeserializer<IHTTPRequest> {
+
+			@Override
+			public IHTTPRequest deserialize(JsonElement element, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+				return context.deserialize(element, HTTPFactory.getHTTPRequestImplClass());
+			}
+
+		}
+
+		/**
+		 * Adapter class for GSON deserialization that maps the IHTTPResponse
+		 * interface with its implementation.
+		 * @author siddharth.s
+		 */
+		private final static class HTTPResponseAdapter implements JsonDeserializer<IHTTPResponse> {
+
+			@Override
+			public IHTTPResponse deserialize(JsonElement element, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+				return context.deserialize(element, HTTPFactory.getHTTPResponseImplClass());
+			}
+
+		}
+
+		/**
+		 * Adapter class for GSON deserialization that maps the IHTTPPerformance
+		 * interface with its implementation.
+		 * @author siddharth.s
+		 */
+		private final static class HTTPPerformanceAdapter implements JsonDeserializer<IHTTPPerformance> {
+
+			@Override
+			public IHTTPPerformance deserialize(JsonElement element, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+				return context.deserialize(element, HTTPFactory.getHTTPPerformanceImplClass());
+			}
+
+		}
+
+		/**
+		 * Adapter class for GSon deserialization of a generic JSON element.
+		 * @author siddharth.s
+		 */
+		private final static class JsonElementAdapter implements JsonDeserializer<JsonElement> {
+
+			@Override
+			public JsonElement deserialize(JsonElement json, Type type,
+					JsonDeserializationContext context) throws JsonParseException {
+				return json;
+			}
+
+		}
 	}
 
 }
